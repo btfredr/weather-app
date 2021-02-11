fe106d90b6404f90aaa8593872c84882eccdec30
Fredrik <fredrikf2000@gmail.com>
2021-02-11 19:04:09 +0100

    style: added basic components and styling

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+const api = {
+  key: "49b18f35a651493ef9d1cf84f1ad18d9",
+  base: "https://api.openweathermap.org/data/2.5"
+}
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <main>
+        <div className='search-bar'>
+          <input type='text' className='search-box' placeholder='Search...'></input>
+        </div>
+
+      </main>
     </div>
   );
 }
