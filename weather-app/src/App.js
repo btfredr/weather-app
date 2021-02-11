50c4127a221326654e0aa58b3a7ebb783b51486e
Fredrik <fredrikf2000@gmail.com>
2021-02-11 19:21:28 +0100

    app: added ability to get current date and year

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,13 +6,38 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
   return (
     <div className="App">
       <main>
         <div className='search-box'>
           <input type='text' className='search-bar' placeholder='Search...'></input>
         </div>
+        <div className='location-box'>
 
+          <div className='location'>New York City, US</div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+
+        </div>
+        <div className='weather-box'>
+
+          <div className='temp'>15°C</div>
+          <div className='weather'>Sunny</div>
+
+        </div>
       </main>
     </div>
   );
